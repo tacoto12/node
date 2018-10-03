7a0451a674e31a555bc4081547da151f1cbbb419
Ruben Bridgewater <ruben@bridgewater.de>
2018-10-03 19:16:41 +0200

    domain: minify unused domain properties
    Domain objects can be very verbose when inspecting them. To reduce
    that overhead the objects are now minified if not used.

--- test/parallel/test-domain-timer.js
+++ test/parallel/test-domain-timer.js
@@ -3,6 +3,7 @@
 const common = require('../common');
 const assert = require('assert');
 const domain = require('domain');
+const util = require('util');
 
 const d = new domain.Domain();
 
@@ -12,10 +13,12 @@ d.on('error', common.mustCall((err) => {
   assert.strictEqual(err.domainEmitter, undefined);
   assert.strictEqual(err.domainBound, undefined);
   assert.strictEqual(err.domainThrown, true);
+  assert(/domain:\n   Domain {/.test(util.inspect(err)));
 }));
 
 d.run(common.mustCall(() => {
-  setTimeout(common.mustCall(() => {
+  const timeout = setTimeout(common.mustCall(() => {
     throw new Error('foobar');
   }), 1);
+  assert(/domain: \[Domain]/.test(util.inspect(timeout)));
 }));
